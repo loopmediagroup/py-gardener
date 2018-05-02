a5ee2f9f463b61e0f5bed6cec334a14e91e7aa07
Lukas Siemon <lukas.siemon@getintheloop.ca>
2018-05-02 14:59:02 -0700

    - initial commit

---
+++ run_tests.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import pytest
+
+
+def run_tests():
+    args = [
+        '--cov=lib',
+        '--cov=tests',
+        '--cov-report=html',
+        '--cov-report=term-missing:skip-covered',
+        '--cov-config=.coveragerc'
+    ]
+    if '--verbose' in sys.argv:
+        args.append('-s')
+    if '--batch' in sys.argv:
+        os.environ['BATCH_RUN'] = "1"
+    pytest.main(args)
+
+
+if __name__ == '__main__':
+    run_tests()
