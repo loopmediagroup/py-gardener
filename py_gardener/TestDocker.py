093f71ea802655fb925dc132012d1e81a70a2014
jeffsoloshy@gmail.com <eb4cdf0b742fed712b6f983a36b19896a9735da9>
2018-05-09 10:16:33 -0700

    chore(docker): refactored test, added error message

--- py_gardener/TestDocker.py
+++ py_gardener/TestDocker.py
@@ -5,5 +5,6 @@ from py_gardener.InternalTestBase import InternalTestBase
 class TestDocker(InternalTestBase):
 
     def test_docker(self):
-        if "lambda" in self.DOCKER:
-            assert os.path.exists(os.path.join("/.dockerenv"))
+        if len(self.DOCKER) > 0:
+            assert os.path.exists(os.path.join("/.dockerenv")), (
+                'Please run in Docker using ". manage.sh"')
