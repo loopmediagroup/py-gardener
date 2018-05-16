f2af676fd46d94b42cd3084860c03ff50980148e
jeffsoloshy@gmail.com <eb4cdf0b742fed712b6f983a36b19896a9735da9>
2018-05-16 10:33:29 -0700

    feat: Added max-line-length as variable #37

--- py_gardener/TestPep8.py
+++ py_gardener/TestPep8.py
@@ -6,6 +6,10 @@ class TestPep8(InternalTestBase):
     """ Test that we conform to PEP8. """
 
     def test_pep8(self):
-        pep8style = pycodestyle.StyleGuide(quiet=False)
+        line_length = self.OPTIONS.get('max-line-length', 80)
+        pep8style = pycodestyle.StyleGuide(
+            quiet=False,
+            paths=['--max-line-length=%s' % line_length]
+        )
         result = pep8style.check_files(self.list_project_files())
         assert result.total_errors == 0, "Found pep8 problems"
