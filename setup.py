48361f89f83cb1ebdeb4b91154172d2e1dfc831f
Lukas Siemon <lukas.siemon@getintheloop.ca>
2018-07-17 10:59:24 -0700

    amend(fix): updated setup.py version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup
 
 setup(
     name='py-gardener',
-    version='0.6.4',
+    version='0.6.5',
     url='https://github.com/loopmediagroup/py-gardener',
     license='MIT',
     author='Lukas Siemon',
